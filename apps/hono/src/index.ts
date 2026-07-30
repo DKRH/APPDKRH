439d593d2be25e98bbe300cea0fb29c880323278
DKRH <hasegawakyousuke87@gmail.com>
2026-07-30 18:41:39 +0700

    fix hono index

--- apps/hono/src/index.ts
+++ apps/hono/src/index.ts
@@ -4,32 +4,43 @@ import auth from "./routeAuth/auth";
 import { authMiddleware } from "./middleware/auth";
 import { cors } from "hono/cors";
 import { protectedApi } from "./routes";
+import { dirname, resolve } from "node:path";
+
+const baseDir =
+  process.env.APP_ENV === "development"
+    ? resolve(process.cwd(), "../..") // monorepo root
+    : dirname(process.execPath);      // compiled executable directory
+
+const indexFile = Bun.file(resolve(baseDir, "index.html"));
 
 const app = new Hono();
 
 protectedApi.use("*", authMiddleware);
 
-app.use( "/*", serveStatic({ root: "./", }) );
 app.use(
   "*",
   cors({
     origin: "http://localhost:2601",
-    allowMethods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-      "OPTIONS",
-    ],
-    allowHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
+
 app.route("/api/auth", auth);
 app.route("/api", protectedApi);
+
+// Static files
+app.use("/*", serveStatic({ root: baseDir }));
+
+// SPA fallback
+app.notFound(async (c) => {
+  if (c.req.path.startsWith("/api")) {
+    return c.text("Not Found", 404);
+  }
+
+  return c.html(await indexFile.text());
+});
 
 export default {
 	port: 2600,
