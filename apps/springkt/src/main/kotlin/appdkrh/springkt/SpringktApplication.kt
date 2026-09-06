f4c0a8816edcddad9459dfbd7ecdc9404bf7ea43
DKRH <hasegawakyousuke87@gmail.com>
2026-09-06 08:48:01 +0700

    adding Spring Kotlin

---
+++ apps/springkt/src/main/kotlin/appdkrh/springkt/SpringktApplication.kt
@@ -0,0 +1,11 @@
+package appdkrh.springkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringktApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringktApplication>(*args)
+}
---
+++ apps/springkt/src/test/kotlin/appdkrh/springkt/SpringktApplicationTests.kt
@@ -0,0 +1,13 @@
+package appdkrh.springkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringktApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
